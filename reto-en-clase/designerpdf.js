7ef2f2152f5ff746485408c72bade16fa8fdab31
unknown <calbodragon>
2019-07-05 15:05:21 -0500

    up

---
+++ reto-en-clase/designerpdf.js
@@ -0,0 +1,32 @@
+
+
+
+ var word="acb";
+var h = [1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5];
+
+function designerPdfViewer(h, word) {
+var altura=h;
+var palabra=word;
+
+for (i=0;i<26;i++) {
+    diccioinario [ charart(i) ]= altura[i]
+}
+
+return altura;
+}
+
+
+//
+
+
+
+
+
+
+ 
+
+
+
+
+
+   
