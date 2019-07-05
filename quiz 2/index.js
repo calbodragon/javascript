baab01c1a930b4fba010c34bcb75183cb97ca795
unknown <calbodragon>
2019-07-05 15:45:48 -0500

    upload

---
+++ quiz 2/index.js
@@ -0,0 +1,18 @@
+// declarar variables
+
+var metros=0;
+var kilometros=0;
+var decimetros= 0;
+
+//imprimir variable
+
+console.log(metros+kilometros+decimetros);
+
+
+//condicionales
+
+if (metros %2 == 0){
+    kilometros =metros/1000
+}else(decimetros=metros *10){
+    }
+ console.log(decimetros+"decimetros")
