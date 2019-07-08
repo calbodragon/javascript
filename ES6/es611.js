525782c0bbffb150c806ca6a1baaf4e603ae05d2
unknown <calbodragon>
2019-07-08 04:15:09 -0500

    up

---
+++ ES6/es611.js
@@ -0,0 +1,8 @@
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2=[...arr1];
+(function() {
+  "use strict";
+   
+   let spreaded =Math.max (...arr1) // change this line
+})();
+console.log(arr2);
