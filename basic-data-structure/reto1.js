9f6cf8b8692d47247e13c03dff4f8f5e96973494
unknown <calbodragon>
2019-07-08 12:57:08 -0500

    up

---
+++ basic-data-structure/reto1.js
@@ -0,0 +1,4 @@
+let yourArray=
+[,5,"mar",false,"love",2,6];
+console.log(yourArray.length);
+ // change this line
---
+++ ejerccios_funciones_arreglos/funciones.js
@@ -0,0 +1,10 @@
+let x =[2,4,6,8,10];
+//f(x);
+//crear la funcion
+console.table(`el resultado es $f(x)`)
+function f(x){
+  let y =5*(x)+2;
+  console.log("el resultado es" + y)  
+}
+//llamar la funcion
+console.log("el resultado es "+f(x))
